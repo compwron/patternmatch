5127e22c724ec442c12d40de9bee909fbedd018b
compwron <compiledwrong@gmail.com>
2015-10-20 02:19:34 -0700

    removing cruft; no change in test results

--- foo.rb
+++ foo.rb
@@ -1,45 +1,22 @@
 require 'pry'
+
+def contains_repeated_substrings?(input)
+  a = input.match(/(.+)\1+/)
+  a != nil && a[0] != nil
+end
+
 def wordpattern(pattern, input)
-  # binding.pry
   pattern_parts = pattern.split('')
   pattern_count = pattern_parts.count
 
 
   input_parts = input.split('')
   input_count = input_parts.count
-  if (input_count % pattern_count) == 0
-      pattern_map = {}
-      pattern_parts.each {|i|
-      pattern_map[i] ||= 0
-      pattern_map[i] += 1
-    }
-
-    input_map = {}
-    input_equivalent_length = input_count / pattern_count
-
-    clone_input = input_parts.clone
-
-    input_sized_chunks = []
-
-    upto = 0
-    input_chunks = 0.upto(input_count / pattern_count).map {|i|
-      # puts "upto: #{upto}, #{upto + input_equivalent_length}"
-      # ruby substring is start and length, not start and end
-      a = clone_input[upto, input_equivalent_length].join('')
-      # p a
-      upto += input_equivalent_length
-      a
-    }
-
-    #  a b b a
-    # foo bar bar foo
+  if (contains_repeated_substrings?(input)) == 0
 
     pattern_parts.each_with_index {|i, index_i|
       pattern_parts.each_with_index {|j, index_j|
-        # binding.pry
-        # p pattern_parts
-        # p input_chunks
-        # binding.pry
+
         if pattern_parts[index_i] == pattern_parts[index_j]
           if input_chunks[index_i] == input_chunks[index_j]
             # p "YES; keep going"
@@ -60,6 +37,21 @@ def wordpattern(pattern, input)
     1
   else
     0
+  end
+end
+
+def all_possible_next_chunks(remaining_length, remaining_chars, possible_chunks=[])
+  # possible_chunks = [[aa, bb], [a, abb], [a, ab, b]]
+  if remaining_length == 0
+    return possible_chunks
+  else
+    1.upto(remaining_length).each { |length|
+      possible_chunks.each { |pc|
+        pc
+      }
+      remaining_length = input_count - length
+
+    }
   end
 end
 
